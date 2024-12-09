5c29b7b9022274e34d076ac04092a6a588c64ebe
rommill <rommill19@gmail.com>
2024-12-09 23:04:21 +0200

    commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import {Main} from "./layout/header/sections/main/Main";
 import {Skills} from "./layout/header/sections/skills/Skills";
 import {Works} from "./layout/header/sections/works/Works";
 import {Testimony} from "./layout/header/sections/testimony/Testimony";
+import {Contact} from "./layout/header/sections/contacts/Contacts";
+import {Slogan} from "./layout/header/sections/slogan/Slogan";
+import {Footer} from "./layout/footer/Footer";
 
 function App() {
     return (
@@ -13,6 +16,9 @@ function App() {
             <Skills/>
             <Works/>
             <Testimony/>
+            <Contact/>
+            <Slogan/>
+            <Footer/>
         </div>
     );
 }
