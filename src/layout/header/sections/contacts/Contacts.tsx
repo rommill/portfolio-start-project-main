8d72b98e498c733955f14858b73ae7be6abf8d50
rommill <rommill19@gmail.com>
2024-12-17 23:37:41 +0200

    commit - 1

--- src/layout/header/sections/contacts/Contacts.tsx
+++ src/layout/header/sections/contacts/Contacts.tsx
@@ -2,34 +2,62 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../../assets/components/SectionTitle";
 import {Button} from "../../../../assets/components/Button";
+import {Container} from "../../../../assets/components/Container";
+import {theme} from "../../../../styles/Theme";
 
 export const Contact = () => {
     return (
         <StyledContacts>
-<SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"name"}/>
-                <Field placeholder={"subject"}/>
-                <Field placeholder={"message"} as={"textarea"}/>
-                <Button type={"submit"}>Send message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={"name"}/>
+                    <Field placeholder={"subject"}/>
+                    <Field placeholder={"message"} as={"textarea"}/>
+                    <Button type={"submit"}>Send message</Button>
+                </StyledForm>
+            </Container>
+
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section`
-    min-height: 50vh;
-    background-color: cadetblue;
+  
 `
 const StyledForm = styled.form`
-    max-width: 500px;
+    max-width: 540px;
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    align-items: center;
+    gap: 16px;
     margin: 0 auto;
+    
+    textarea {
+        resize: none;
+        height: 155px;
+    }
 `
 
 const Field = styled.input`
+    width: 100%;
+    background-color: ${theme.colors.secondaryBg};
+    border: 1px solid ${theme.colors.borderColor};
+    padding: 7px 15px;
     
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: 0.05em;
+    
+    color: ${theme.colors.font};
+    &::placeholder {
+        color: ${theme.colors.placeholderColor};
+        text-transform: capitalize;
+        
+        &:focus-visible {
+            outline: 1px solid ${theme.colors.borderColor};
+        }
+    }
 `
