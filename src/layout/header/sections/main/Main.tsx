4788d113440b0fecf9639703ed60f5d07a740bbe
rommill <rommill19@gmail.com>
2024-12-20 22:28:26 +0200

    commit - 1

--- src/layout/header/sections/main/Main.tsx
+++ src/layout/header/sections/main/Main.tsx
@@ -2,21 +2,53 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../../assets/images/photo.webp'
 import {FlexWrapper} from "../../../../assets/components/FlexWrapper";
+import {FotoWrapper} from "../../../../assets/components/FlexWrapper";
+import {Container} from "../../../../assets/components/Container";
+import {theme} from "../../../../styles/Theme";
+import {font} from "../../../../styles/Common";
 
 export const Main = () => {
     return (
 
        <div>
            <StyledMain>
-               <FlexWrapper align={"center"} justify={"space-around"}>
-                   <div>
-                       <span>Hi There</span>
-                       <Name>I am Roman Filippov</Name>
-                       <MainTitle>A Web Developer</MainTitle>
-                   </div>
+               <Container>
+                   <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
+                       <div>
+                           <SmallText>Hi There</SmallText>
+                           <Name>I am Roman Filippov</Name>
+                           <MainTitle>A Web Developer</MainTitle>
+                       </div>
 
-                   <Photo src={photo} alt="" />
-               </FlexWrapper>
+                       <FotoWrapper>
+
+                           <div className="card">
+                               <div className="card-inner">
+                                   <div className="card-front">
+                                            {/*// ostavit tolko eto*/}
+                                           <PhotoWrapper>
+                                               <Photo src={photo} alt=""/>
+                                           </PhotoWrapper>
+
+                                   </div>
+                                   <div className="card-back">
+
+                                           <PhotoWrapper>
+                                               <Photo src={photo} alt=""/>
+                                           </PhotoWrapper>
+
+                                   </div>
+                               </div>
+                           </div>
+
+                       </FotoWrapper>
+                       {/*<PhotoWrapper>*/}
+                       {/*    <Photo src={photo} alt=""/>*/}
+                       {/*</PhotoWrapper>*/}
+
+                   </FlexWrapper>
+               </Container>
+
            </StyledMain>
        </div>
 
@@ -25,16 +57,84 @@ export const Main = () => {
 };
 
 const StyledMain = styled.div`
-min-height: 100vh;
+    min-height: 100vh;
     background-color: bisque;
-`
-const Photo = styled.img`
-width: 350px;
-    height: 430px;
-    object-fit: cover;
-`
-const MainTitle = styled.h1`
+    display: flex;
 
 `
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    margin-top: 65px;
+
+
+    &::before {
+        content: "";
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -17px;
+            left: 20px;
+        }
+
+    }
+`
+
+const Photo = styled.img`
+  width: 100%;
+    object-fit: cover;
+    margin-right: 20px;
+    
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 430px;
+    }
+`
+const MainTitle = styled.h1`
+//font-weight: 400;
+    //font-size: 27px;
+    ${font( {weight:400, Fmax: 27, Fmin: 20})}
+    
+`
+
 const Name = styled.h2`
+    ${font( {family: "'Josefin Sans', sans-serif",weight:700, Fmax: 50, Fmin: 36})}
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+    
+    span {
+        position: relative;
+        z-index: 0;
+        white-space: nowrap;
+        
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
+    }
+`
+
+const SmallText = styled.h2`
+    font-weight: 400;
+    font-size: 14px;
+    ${font( {weight:400, Fmax: 27, Fmin: 20})}
 `
